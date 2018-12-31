e7b3b36fb333975e400178a4e49823d1c953fdef
alvin <alvin.leung92@gmail.com>
2018-12-30 21:03:24 -0800

    Implement route resolver for member details page

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -21,6 +21,7 @@ import { AuthGuard } from './_guards/auth.guard';
 import { UserService } from './_services/user.service';
 import { MemberCardComponent } from './members/member-card/member-card.component';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
+import { MemberDetailResolver } from './_resolvers/member-detail-resolver';
 
 export function tokenGetter() {
    return localStorage.getItem('token');
@@ -58,7 +59,8 @@ export function tokenGetter() {
       ErrorInterceptorProvider,
       AlertifyService,
       AuthGuard,
-      UserService
+      UserService,
+      MemberDetailResolver
    ],
    bootstrap: [
       AppComponent
--- DatingApp-SPA/src/app/members/member-detail/member-detail.component.ts
+++ DatingApp-SPA/src/app/members/member-detail/member-detail.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { UserService } from 'src/app/_services/user.service';
 import { User } from 'src/app/_models/user';
 import { ActivatedRoute } from '@angular/router';
-import { AlertifyService } from 'src/app/_services/alertify.service';
 
 @Component({
   selector: 'app-member-detail',
@@ -12,20 +10,11 @@ import { AlertifyService } from 'src/app/_services/alertify.service';
 export class MemberDetailComponent implements OnInit {
   user: User;
 
-  constructor(
-    private route: ActivatedRoute,
-    private userService: UserService,
-    private alertifyService: AlertifyService) { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
-    this.loadUser();
-  }
-
-  private loadUser() {
-    this.userService.getUser(+this.route.snapshot.params['id']).subscribe((user: User) => {
-      this.user = user;
-    }, error => {
-      this.alertifyService.error(error);
+    this.route.data.subscribe(data => {
+      this.user = data['user'];
     });
   }
 }
