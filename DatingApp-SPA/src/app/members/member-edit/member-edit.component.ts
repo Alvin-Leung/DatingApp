4c1ccff7566bb84affdd39e5e074d0a3a11d0f1d
alvin <alvin.leung92@gmail.com>
2019-01-01 15:43:20 -0800

    Add ability to update user from SPA

--- DatingApp-SPA/src/app/members/member-edit/member-edit.component.ts
+++ DatingApp-SPA/src/app/members/member-edit/member-edit.component.ts
@@ -3,6 +3,8 @@ import { ActivatedRoute } from '@angular/router';
 import { User } from 'src/app/_models/user';
 import { AlertifyService } from 'src/app/_services/alertify.service';
 import { NgForm } from '@angular/forms';
+import { UserService } from 'src/app/_services/user.service';
+import { AuthService } from 'src/app/_services/auth.service';
 
 @Component({
   selector: 'app-member-edit',
@@ -13,7 +15,11 @@ export class MemberEditComponent implements OnInit {
   @ViewChild('editForm') editForm: NgForm;
   user: User;
 
-  constructor(private route: ActivatedRoute, private alertify: AlertifyService) { }
+  constructor(
+    private route: ActivatedRoute,
+    private alertify: AlertifyService,
+    private userService: UserService,
+    private authService: AuthService) { }
 
   @HostListener('window:beforeunload', ['$event'])
   unloadNotification($event: any) {
@@ -29,7 +35,11 @@ export class MemberEditComponent implements OnInit {
   }
 
   updateUser() {
-    this.alertify.success('Profile updated successfully!');
-    this.editForm.reset(this.user);
+    this.userService.updateUser(this.authService.decodedToken.nameid, this.user).subscribe(next => {
+      this.alertify.success('Profile updated successfully!');
+      this.editForm.reset(this.user);
+    }, error => {
+      this.alertify.error('error');
+    });
   }
 }
